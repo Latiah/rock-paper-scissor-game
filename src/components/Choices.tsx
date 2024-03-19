d2dc62533881b60d31bc3bad6026bc27576b593d
latifah <kimtifah2@gmail.com>
2024-03-19 12:54:04 +0200

    added readme

---
+++ src/components/Choices.tsx
@@ -0,0 +1,11 @@
+const Choices = () => {
+  return (
+    <>
+      <div className="choice-messages">
+        <h3>YOU PICKED</h3>
+        <h3>THE HOUSE PICKED</h3>
+      </div>
+    </>
+  );
+};
+export default Choices;
